2bf89e9690117c0168565c7987a2b953cf2af89f
Marek Štrba <marek.strbaa@gmail.com>
2019-09-19 19:08:11 +0200

    Defrag

--- 1_zadanie.c
+++ 1_zadanie.c
@@ -27,6 +27,47 @@ chunk* find_free_chunk(unsigned int size){
     
 }
 
+void defrag(chunk *first){
+    
+    chunk *mover=first;
+    chunk *place_holder;
+    chunk *allocated_first;
+    chunk *allocated;
+    chunk *free_chunk;
+    
+    unsigned int free_space;
+    
+    while (mover!=NULL){
+        if(mover->free){
+            if(free_chunk==NULL){
+                free_chunk=mover;
+                free_chunk->next=NULL;
+            }else{
+                mover->prev->next=mover->next;
+                free_chunk->chunk_size+=mover->chunk_size;
+            }
+        }else{
+            if(allocated_first==NULL){
+                allocated_first=mover;
+            }else{
+                if(allocated==NULL){
+                    allocated=mover;
+                    allocated->prev=allocated_first;
+                    allocated_first->next=allocated;
+                }else{
+                    allocated->next=mover;
+                    allocated=mover;
+                }
+            }
+        }
+        if(mover->next==NULL){
+            allocated->next=free_chunk;
+            free_chunk->prev=allocated;
+        }
+    }
+    
+}
+
 
 void *memory_alloc(unsigned int size){
     
